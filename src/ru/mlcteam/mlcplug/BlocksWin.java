14727251ab45f105dc0facc2e14ee6fd06e9e94d
Binary3310 <33326440+Binary3310@users.noreply.github.com>
2021-01-31 15:18:35 +0300

    Delete src/ru/mlcteam/mlcplug directory

--- src/ru/mlcteam/mlcplug/BlocksWin.java
+++
@@ -1,9 +0,0 @@
-package ru.mlcteam.mlcplug;
-
-public interface BlocksWin {
-    int[] blocks = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-    // 1;1  0;1 -1;1
-    // 1;0  0;0 -1;0
-    // 1;-1 0;-1 -1;-1
-    // ALL Y'S: 24
-}
--- src/ru/mlcteam/mlcplug/onlineplayers/OnlinePlayers.java
+++
@@ -1,9 +0,0 @@
-package ru.mlcteam.mlcplug.onlineplayers;
-
-import org.bukkit.entity.Player;
-
-import java.util.HashMap;
-
-public interface OnlinePlayers {
-    HashMap<Player, OnlinePlayerInfo> players = new HashMap<>();
-}
